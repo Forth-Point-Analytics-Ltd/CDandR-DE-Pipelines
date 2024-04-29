b58f7a9195fa666a78208545f0e4d7931f3072fd
Logi Gunaratnam <logi.gunaratnam@forthpoint.com>
2024-04-29 09:40:37 +0100

    Include bronze to silver spark transformation comment out demo integration tests

--- src/transformations/to_silver/savers/bronze_to_silver.py
+++ src/transformations/to_silver/savers/bronze_to_silver.py
@@ -4,6 +4,21 @@ from src.config import logger
 from src.transformations.base_transformer import BaseTransformer
 from src.utils.types import DataFrameProperties
 from src.utils.preprocessing.transformation import apply_prefix
+from pyspark.sql.functions import (
+    col,
+    regexp_extract,
+    when,
+    lit,
+    lower,
+    regexp_replace,
+)
+from pyspark.sql.types import (
+    DecimalType,
+    DoubleType,
+    IntegerType,
+    TimestampType,
+    StringType,
+)
 
 
 class BronzeToSilver(BaseTransformer):
@@ -30,9 +45,156 @@ class BronzeToSilver(BaseTransformer):
         self.table_transform_config = table_transform_config
         self.src_database = src_database
         self.dst_database = dst_database
+        self.product_pattern = r"(?i)(\b\d+(?:\s*[x×]\s*\d+)?\.?\d*)\s?(ml|cl|g|kg|l|s|pk|litre|pack|pads|'s)\b"
+        self.product_single_multiplier_pattern = r"[xX]\s*(\d+)"
+        self.product_multiplier_left_pattern = r"(\d+) x"
+        self.product_multiplier_right_pattern = r"x (\d+)"
 
     def savers_transformation(self, df: DataFrame) -> DataFrame:
-        return df
+        product_name_split_df = (
+            df.withColumn(
+                "q1",
+                regexp_extract(col("product_name"), self.product_pattern, 0),
+            )
+            .withColumn(
+                "product_size",
+                regexp_extract(col("product_name"), self.product_pattern, 1),
+            )
+            .withColumn(
+                "product_size_unit",
+                regexp_extract(col("product_name"), self.product_pattern, 2),
+            )
+            .withColumn(
+                "q1",
+                when(
+                    col("q1") == "",
+                    regexp_extract(
+                        col("product_name"),
+                        self.product_single_multiplier_pattern,
+                        0,
+                    ),
+                ).otherwise(col("q1")),
+            )
+            .withColumn(
+                "product_size",
+                when(
+                    col("product_size") == "",
+                    regexp_extract(
+                        col("product_name"),
+                        self.product_single_multiplier_pattern,
+                        1,
+                    ),
+                ).otherwise(col("product_size")),
+            )
+            .withColumn(
+                "product_quantity",
+                when(
+                    lower(col("product_size")).contains(lit("x")),
+                    regexp_extract(
+                        col("product_size"),
+                        self.product_multiplier_left_pattern,
+                        1,
+                    ),
+                ).otherwise(lit("1")),
+            )
+            .withColumn(
+                "product_size",
+                when(
+                    lower(col("product_size")).contains(lit("x")),
+                    regexp_extract(
+                        col("product_size"),
+                        self.product_multiplier_right_pattern,
+                        1,
+                    ),
+                ).otherwise(col("product_size")),
+            )
+            .withColumn(
+                "product_size",
+                when(col("product_size") == "", lit("1")).otherwise(
+                    col("product_size")
+                ),
+            )
+            .withColumn(
+                "product_size_unit",
+                when(col("product_size_unit") == "", lit("ea")).otherwise(
+                    col("product_size_unit")
+                ),
+            )
+        )
+        clean_product_name_split_df = (
+            product_name_split_df.withColumn(
+                "product_size",
+                when(
+                    (lower(col("product_size_unit")).contains("litre"))
+                    | (lower(col("product_size_unit")) == ("l")),
+                    col("product_size").cast(DoubleType())
+                    * lit(1000).cast(IntegerType()),
+                ).otherwise(col("product_size")),
+            )
+            .withColumn("product_size_unit", lower(col("product_size_unit")))
+            .withColumn(
+                "product_size_unit",
+                when(
+                    (col("product_size_unit").contains("litre"))
+                    | (col("product_size_unit") == ("l")),
+                    lit("ml"),
+                )
+                .when(
+                    (col("product_size_unit") == "s")
+                    | (col("product_size_unit") == "S")
+                    | (col("product_size_unit") == "'s")
+                    | (col("product_size_unit") == "pk")
+                    | (col("product_size_unit") == "pack")
+                    | (col("product_size_unit") == "pads"),
+                    lit("ea"),
+                )
+                .otherwise(col("product_size_unit")),
+            )
+            .withColumn(
+                "product_price_per_unit",
+                when((col("product_size_unit") == "g"), lit("100g"))
+                .when((col("product_size_unit") == "ml"), lit("100ml"))
+                .when(col("product_size_unit") == "ea", lit("ea"))
+                .otherwise(lit(None)),
+            )
+            .withColumn(
+                "product_price_per",
+                when(
+                    (col("product_size_unit") == "g")
+                    | (col("product_size_unit") == "ml"),
+                    (
+                        col("product_price_now").cast(DoubleType())
+                        / col("product_size").cast(DoubleType())
+                    )
+                    * (
+                        lit(100) / (col("product_quantity").cast(DoubleType()))
+                    ),
+                )
+                .when(
+                    col("product_size_unit") == "ea",
+                    (
+                        col("product_price_now").cast(DoubleType())
+                        / col("product_size").cast(DoubleType())
+                    )
+                    * (lit(1) / (col("product_quantity").cast(DoubleType()))),
+                )
+                .otherwise(lit(None)),
+            )
+        ).select(
+            regexp_replace(col("product_name"), "&amp;", "&")
+            .cast(StringType())
+            .alias("product_name"),
+            col("brand_name").cast(StringType()),
+            col("url").cast(StringType()),
+            col("product_price_now").cast(DecimalType(scale=2)),
+            col("product_quantity").cast(IntegerType()),
+            col("product_size").cast(IntegerType()),
+            col("product_size_unit").cast(IntegerType()),
+            col("product_price_per").cast(DecimalType(scale=2)),
+            col("product_price_per_unit").cast(StringType()),
+        )
+
+        return clean_product_name_split_df
 
     def transform(self) -> List[DataFrameProperties]:
         dfs = []
